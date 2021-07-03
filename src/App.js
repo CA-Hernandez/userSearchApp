f62618b60c2e995bc979e60f14348c9847037e50
Jaybasto <80229726+Jaybasto@users.noreply.github.com>
2021-07-02 22:12:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Header from "./component/header"
+import Table from "./component/header/Table.js";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <center>
+    <Table/>
+    </center>
     </div>
   );
 }
